ce594dc4b577fd9dbe7bcfb7217ed1be8638c9a9
Happy <pmp0606@mail.ru>
2019-07-24 00:12:19 +0300

    Change Account service.

--- src/main/java/com/potapov/bankList/service/AccountService.java
+++ src/main/java/com/potapov/bankList/service/AccountService.java
@@ -1,133 +1,105 @@
 package com.potapov.bankList.service;
 
-import com.potapov.bankList.connection.BankListJDBC;
-import com.potapov.bankList.dao.AccountDao;
 import com.potapov.bankList.entity.Account;
+import com.potapov.bankList.repository.AccountRepository;
+import com.potapov.bankList.repository.UserRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-public class AccountService extends BankListJDBC implements AccountDao {
+public class AccountService  {
 
-    private Connection connection = getConnection();
-    private PreparedStatement preparedStatement = null;
-    private UserService userService = new UserService();
+    private AccountRepository accountRepository;
+    private UserRepository userRepository;
 
-    @Override
-    public Boolean add(Account account) {
-        String sql = "INSERT INTO ACCOUNT(ACCOUNTID, ACCOUNT, USERID) VALUES(?,?,?)";
+    Account getById(int accountId) throws Exception {
 
-        try {
-            preparedStatement = connection.prepareStatement(sql);
+        if (accountId >= 0) {
 
-            preparedStatement.setInt(1, account.getAccountId());
-            preparedStatement.setInt(2, account.getAccount());
-            preparedStatement.setInt(3, account.getUserId());
+            Account user = accountRepository.getById(accountId);
 
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            userService.closingConnectionAndStatement();
-        }
-        return true;
-    }
+            if (user != null) {
+                return user;
 
-    @Override
-    public List<Account> getAll() {
-
-        List<Account> accountList = new ArrayList<>();
-        String sql = "SELECT * FROM ACCOUNT";
-
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()){
-                Account account = new Account();
-                account.setAccountId(resultSet.getInt("ACCOUNTID"));
-                account.setAccount(resultSet.getInt("ACCOUNT"));
-                account.setUserId(resultSet.getInt("USERID"));
-
-                accountList.add(account);
+            } else {
+                throw new ResourceNotFoundException("Account with id: " + accountId + " is not found.");
             }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            userService.closingConnectionAndStatement();
+        } else {
+            throw new RuntimeException("Id is not valid.");
         }
 
-        return accountList;
     }
 
-    @Override
-    public Account getById(int accountId) {
-        String sql = "SELECT * FROM ACCOUNT WHERE ACCOUNTID = ?";
-        Account account = new Account();
 
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, accountId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+    List<Account> getAll(){
+        return accountRepository.getAll();
+    }
 
-            account.setAccountId(resultSet.getInt("ACCOUNTID"));
-            account.setAccount(resultSet.getInt("ACCOUNT"));
-            account.setUserId(resultSet.getInt("USERID"));
 
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            userService.closingConnectionAndStatement();
+    Account add(Account account){
+
+        if(userRepository.getById(account.getUserId()) != null){
+
+            if (accountRepository.getById(account.getAccountId()) == null){
+
+                accountRepository.add(account);
+
+            } else {
+
+                throw new RuntimeException("Account is already exists.");
+            }
+        } else {
+
+            throw new ResourceNotFoundException("User is not found");
+
         }
 
         return account;
     }
 
-
-    @Override
-    public void update(Account account) {
-
-        String sql = "UPDATE ACCOUNT SET ACCOUNT = ?, USERID = ? WHERE ACCOUNTID = ?";
+    Account update(Account account){
 
         try {
-            preparedStatement = connection.prepareStatement(sql);
 
-            preparedStatement.setInt(1, account.getAccount());
-            preparedStatement.setInt(2, account.getUserId());
-            preparedStatement.setInt(3, account.getAccountId());
+            Account accountInstance = getById(account.getUserId());
 
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+            if (accountInstance != null) {
+
+                accountRepository.update(account);
+
+            } else {
+
+                throw new ResourceNotFoundException("Account is not found.");
+
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        finally {
-            userService.closingConnectionAndStatement();
-        }
+
+        return account;
     }
 
-    @Override
-    public Boolean delete(int accountId) {
-        String sql = "DELETE FROM ACCOUNT WHERE ACCOUNTID = ?";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
+    boolean delete(int accountId){
 
-            preparedStatement.setInt(1, accountId);
+        if (accountId >= 0) {
 
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
+            Account account = accountRepository.getById(accountId);
+
+            if (account != null) {
+                accountRepository.delete(accountId);
+
+            } else {
+                throw new ResourceNotFoundException("Account is not found.");
+            }
+
+        } else {
+            throw new RuntimeException("Id is not valid.");
+
         }
-        finally {
-            userService.closingConnectionAndStatement();
-        }
+
         return true;
     }
-
 
 }
